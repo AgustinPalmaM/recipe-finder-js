abc552e0961e010c45a4cb0fcb276e5768157cf3
AgustinPalmaM <palma.agustin7@gmail.com>
2023-03-18 18:12:51 -0300

    Create function showRecipe where we create whole the html to show each recipe in its own bootstrap card

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ function startApp() {
 
   const selectCategories = document.querySelector('#categorias');
   selectCategories.addEventListener( 'change', selectCategory );
+
+  const result = document.querySelector('#resultado');
   
   getCategories();
 
@@ -28,7 +30,54 @@ function startApp() {
     const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${ category }`;
     fetch(url)
       .then(response => response.json())
-      .then(data => console.log(data.meals))
+      .then(data => showRecipe(data.meals))
+  }
+
+  function showRecipe(recipe = []) {
+    cleanHTML(result);
+    recipe.forEach(recipe => {
+      const { idMeal, strMeal, strMealThumb } = recipe;
+
+      const recipeContainer = document.createElement('DIV');
+      recipeContainer.classList.add('col-md-4');
+
+      const recipeCard = document.createElement('DIV');
+      recipeCard.classList.add('card', 'mb-4');
+
+      const recipeImage = document.createElement('IMG');
+      recipeImage.classList.add('card-img-top');
+      recipeImage.alt = `Recipe image ${strMeal}`;
+      recipeImage.src = strMealThumb;
+
+      const recipeBody = document.createElement('DIV');
+      recipeBody.classList.add('card-body');
+      
+      const recipeHeading = document.createElement('H3');
+      recipeHeading.classList.add('card-title', 'mb-3');
+      recipeHeading.textContent = strMeal;
+      
+      const recipeButton = document.createElement('BUTTON');
+      recipeButton.classList.add('btn', 'btn-danger', 'w-100');
+      recipeButton.textContent = 'Watch recipe';
+
+      recipeBody.appendChild(recipeHeading);
+      recipeBody.appendChild(recipeButton);
+
+      recipeCard.appendChild(recipeImage);
+      recipeCard.appendChild(recipeBody);
+
+      recipeContainer.appendChild(recipeCard);
+
+      result.appendChild(recipeContainer);
+
+      console.log(recipeButton);
+    })
+  }
+
+  function cleanHTML(element) {
+    while (element.firstChild) {
+      element.removeChild(result.firstChild);
+    }
   }
 }
 
